7af9ab33be47bc1edafb3be00e167b0a1a601c04
Valery Jacobs <valeryjacobs@hotmail.com>
2015-10-23 16:51:49 +0200

    Init

---
+++ QuantifyMe81/MainPage.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Microsoft.Band;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace QuantifyMe81
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private App viewModel;
+        
+        private IBandClient bandClient;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            this.DataContext = this.viewModel = App.Current;
+
+            App.Current.MainWindow = this;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private async void stop_Click(object sender, RoutedEventArgs e)
+        {
+            await bandClient.SensorManager.HeartRate.StopReadingsAsync();
+            this.viewModel.StatusMessage = "Stopped sensing.";
+        }
+
+        private async void start_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.viewModel.StatusMessage = "Starting...";
+
+                IBandInfo[] pairedBands = await BandClientManager.Instance.GetBandsAsync();
+                bandClient = await BandClientManager.Instance.ConnectAsync(pairedBands[0]);
+
+                if (pairedBands.Length < 1)
+                {
+                    this.viewModel.StatusMessage = "Connect your band and click the start button again.";
+                    return;
+                }
+
+                bool heartRateConsentGranted;
+
+                // Check whether the user has granted access to the HeartRate sensor.
+                if (bandClient.SensorManager.HeartRate.GetCurrentUserConsent() == UserConsent.Granted)
+                {
+                    heartRateConsentGranted = true;
+                }
+                else
+                {
+                    heartRateConsentGranted = await bandClient.SensorManager.HeartRate.RequestUserConsentAsync();
+                }
+
+                if (!heartRateConsentGranted)
+                {
+                    this.viewModel.StatusMessage = "Access to the heart rate sensor is denied.";
+                    
+                }
+                else
+                {
+                    // Subscribe to HeartRate data.
+                    bandClient.SensorManager.HeartRate.ReadingChanged += (s, args) =>
+                    {
+                        this.viewModel.StatusMessage = args.SensorReading.HeartRate.ToString();
+                        this.viewModel.Quantification.HearRate = args.SensorReading.HeartRate;
+                    };
+                    await bandClient.SensorManager.HeartRate.StartReadingsAsync();
+                }
+
+                bool gyroConsentGranted;
+
+                // Check whether the user has granted access to the HeartRate sensor.
+                if (bandClient.SensorManager.Accelerometer.GetCurrentUserConsent() == UserConsent.Granted)
+                {
+                    gyroConsentGranted = true;
+                }
+                else
+                {
+                    gyroConsentGranted = await bandClient.SensorManager.Accelerometer.RequestUserConsentAsync();
+                }
+
+                if (!gyroConsentGranted)
+                {
+                    this.viewModel.StatusMessage = "Access to the accelerometer is denied.";
+                }
+                else
+                {
+                    // Subscribe to HeartRate data.
+                    bandClient.SensorManager.Accelerometer.ReadingChanged += (s, args) =>
+                    {
+                        this.viewModel.StatusMessage = args.SensorReading.AccelerationX.ToString();
+
+
+                    };
+
+                    await bandClient.SensorManager.Accelerometer.StartReadingsAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                this.viewModel.StatusMessage = ex.ToString();
+            }
+        }
+    }
+}
---
+++ QuantifyMe81/Quantification.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+
+namespace QuantifyMe81
+{
+    public class Quantification: INotifyPropertyChanged
+    {
+        private int _heartRate;
+        public int HearRate
+        {
+            get { return _heartRate; }
+            set
+            {
+                _heartRate = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private double _accelX;
+        public double AccelX
+        {
+            get { return _accelX; }
+            set
+            {
+                _accelX = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private double _accelY;
+        public double AccelY
+        {
+            get { return _accelY; }
+            set
+            {
+                _accelY = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private double _accelZ;
+        public double AccelZ
+        {
+            get { return _accelZ; }
+            set
+            {
+                _accelZ = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+         public event PropertyChangedEventHandler PropertyChanged;
+        private async void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                });
+            }
+        }   
+
+    }
+}
